b575058207fc6239a010b1960ec5199203b05ceb
Punyal <p.punyal@gmail.com>
2015-10-08 20:50:13 +0200

    wheel loader GUI included
    first version with 3D rendering

--- src/main/java/com/punyal/symbiotic/controllers/SymbIoTicGUIController.java
+++ src/main/java/com/punyal/symbiotic/controllers/SymbIoTicGUIController.java
@@ -23,32 +23,34 @@
  */
 package com.punyal.symbiotic.controllers;
 
-import co.nstant.in.cbor.CborBuilder;
-import co.nstant.in.cbor.CborDecoder;
-import co.nstant.in.cbor.CborEncoder;
-import co.nstant.in.cbor.model.DataItem;
-import com.punyal.symbiotic.Utils.Parsers;
 import static com.punyal.symbiotic.constants.ConstantsGUI.*;
 import com.punyal.symbiotic.core.Core;
 import com.punyal.symbiotic.core.feature.ipso.AccThread;
 import com.punyal.symbiotic.core.feature.ipso.BatteryThread;
 import com.punyal.symbiotic.core.feature.ipso.Strain;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import com.punyal.symbiotic.core.feature.wheelloader.Xform;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import javafx.animation.AnimationTimer;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Side;
+import javafx.scene.DepthTest;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
 import javafx.scene.SnapshotParameters;
+import javafx.scene.SubScene;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -62,7 +64,15 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javax.imageio.ImageIO;
@@ -92,6 +102,7 @@ public class SymbIoTicGUIController implements Initializable {
     public void init() {
         initMainWindow();
         initFeatureIPSO();
+        initFeatureWheelLoader();
     }
 
     /*------------------------------------------------------------------------*/
@@ -190,11 +201,18 @@ public class SymbIoTicGUIController implements Initializable {
     
     
     private final AnimationTimer animatorIPSO = new AnimationTimer() {
+        private double angle = 0;
             @Override
             public void handle(long now) {
                 animateBattery();
                 animateStrain();
                 if (animationActive) animateACC();
+                
+                // wheel-loader
+                
+                moleculeGroup.setRotateY(angle);
+                angle += 1;
+                //System.out.println(angle);
             }
         };
     
@@ -370,4 +388,252 @@ public class SymbIoTicGUIController implements Initializable {
         
     }
     
+
+    /*------------------------------------------------------------------------*/
+    /*                   Feature Wheel Loader  Controllers                    */
+    /*------------------------------------------------------------------------*/
+    
+    @FXML
+    private AnchorPane anchoPaneTest;
+    //private SubScene subSceneWheelLoader;
+    
+    
+    private SubScene subSceneWheelLoader;
+    
+    final Group root = new Group();
+    final Xform world = new Xform();
+    // Camera
+    final PerspectiveCamera camera = new PerspectiveCamera(true);
+    final Xform cameraXform = new Xform();
+    final Xform cameraXform2 = new Xform();
+    final Xform cameraXform3 = new Xform();
+    // Axes
+    final Xform axisGroup = new Xform();
+    // Molecule
+    final Xform moleculeGroup = new Xform();
+    
+    private static final double CAMERA_INITIAL_DISTANCE = -450;
+    private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
+    private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
+    private static final double CAMERA_NEAR_CLIP = 0.1;
+    private static final double CAMERA_FAR_CLIP = 10000.0;
+    private static final double AXIS_LENGTH = 250.0;
+    private static final double HYDROGEN_ANGLE = 104.5;
+    private static final double CONTROL_MULTIPLIER = 0.1;
+    private static final double SHIFT_MULTIPLIER = 10.0;
+    private static final double MOUSE_SPEED = 0.1;
+    private static final double ROTATION_SPEED = 2.0;
+    private static final double TRACK_SPEED = 0.3;
+    
+    double mousePosX;
+    double mousePosY;
+    double mouseOldX;
+    double mouseOldY;
+    double mouseDeltaX;
+    double mouseDeltaY;
+    
+    private void initFeatureWheelLoader() {
+        System.out.println("Loading wheel...");
+        root.getChildren().add(world);
+        root.setDepthTest(DepthTest.ENABLE);
+
+        //buildScene();
+        subSceneWheelLoader = new SubScene(root, 600, 600, true, SceneAntialiasing.BALANCED);
+        anchoPaneTest.getChildren().add(subSceneWheelLoader);
+        //subSceneWheelLoader = new SubScene
+        subSceneWheelLoader.setRoot(root);
+        subSceneWheelLoader.setFill(Color.GREY);
+        buildCamera();
+        subSceneWheelLoader.setCamera(camera);
+        buildAxes();
+        buildMolecule();
+        handleKeyboard(subSceneWheelLoader, root);
+        handleMouse(subSceneWheelLoader, root);
+    }
+    
+    
+    private void buildCamera() {
+        root.getChildren().add(cameraXform);
+        cameraXform.getChildren().add(cameraXform2);
+        cameraXform2.getChildren().add(cameraXform3);
+        cameraXform3.getChildren().add(camera);
+        cameraXform3.setRotateZ(180.0);
+
+        camera.setNearClip(CAMERA_NEAR_CLIP);
+        camera.setFarClip(CAMERA_FAR_CLIP);
+        camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+        cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+        cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+    }
+    
+    private void buildAxes() {
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+ 
+        final PhongMaterial greenMaterial = new PhongMaterial();
+        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+        greenMaterial.setSpecularColor(Color.GREEN);
+ 
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+        blueMaterial.setSpecularColor(Color.BLUE);
+ 
+        final Box xAxis = new Box(AXIS_LENGTH, 1, 1);
+        final Box yAxis = new Box(1, AXIS_LENGTH, 1);
+        final Box zAxis = new Box(1, 1, AXIS_LENGTH);
+        
+        xAxis.setMaterial(redMaterial);
+        yAxis.setMaterial(greenMaterial);
+        zAxis.setMaterial(blueMaterial);
+ 
+        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
+        axisGroup.setVisible(true);
+        world.getChildren().addAll(axisGroup);
+    }
+    
+    
+
+    private void handleMouse(SubScene scene, final Node root) {
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent me) {
+                mousePosX = me.getSceneX();
+                mousePosY = me.getSceneY();
+                mouseOldX = me.getSceneX();
+                mouseOldY = me.getSceneY();
+            }
+        });
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent me) {
+                mouseOldX = mousePosX;
+                mouseOldY = mousePosY;
+                mousePosX = me.getSceneX();
+                mousePosY = me.getSceneY();
+                mouseDeltaX = (mousePosX - mouseOldX); 
+                mouseDeltaY = (mousePosY - mouseOldY); 
+                
+                double modifier = 1.0;
+                
+                if (me.isControlDown()) {
+                    modifier = CONTROL_MULTIPLIER;
+                } 
+                if (me.isShiftDown()) {
+                    modifier = SHIFT_MULTIPLIER;
+                }     
+                if (me.isPrimaryButtonDown()) {
+                    cameraXform.ry.setAngle(cameraXform.ry.getAngle() - mouseDeltaX*MOUSE_SPEED*modifier*ROTATION_SPEED);  
+                    cameraXform.rx.setAngle(cameraXform.rx.getAngle() + mouseDeltaY*MOUSE_SPEED*modifier*ROTATION_SPEED);  
+                }
+                else if (me.isSecondaryButtonDown()) {
+                    double z = camera.getTranslateZ();
+                    double newZ = z + mouseDeltaX*MOUSE_SPEED*modifier;
+                    camera.setTranslateZ(newZ);
+                }
+                else if (me.isMiddleButtonDown()) {
+                    cameraXform2.t.setX(cameraXform2.t.getX() + mouseDeltaX*MOUSE_SPEED*modifier*TRACK_SPEED);  
+                    cameraXform2.t.setY(cameraXform2.t.getY() + mouseDeltaY*MOUSE_SPEED*modifier*TRACK_SPEED);  
+                }
+            }
+        });
+    }
+    
+    private void handleKeyboard(SubScene scene, final Node root) {
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case Z:
+                        cameraXform2.t.setX(0.0);
+                        cameraXform2.t.setY(0.0);
+                        camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+                        cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+                        cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+                        break;
+                    case X:
+                        axisGroup.setVisible(!axisGroup.isVisible());
+                        break;
+                    case V:
+                        moleculeGroup.setVisible(!moleculeGroup.isVisible());
+                        break;
+                }
+            }
+        });
+    }
+    
+    private void buildMolecule() {
+ 
+       final PhongMaterial redMaterial = new PhongMaterial();
+       redMaterial.setDiffuseColor(Color.DARKRED);
+       redMaterial.setSpecularColor(Color.RED);
+ 
+       final PhongMaterial whiteMaterial = new PhongMaterial();
+       whiteMaterial.setDiffuseColor(Color.WHITE);
+       whiteMaterial.setSpecularColor(Color.LIGHTBLUE);
+ 
+       final PhongMaterial greyMaterial = new PhongMaterial();
+       greyMaterial.setDiffuseColor(Color.DARKGREY);
+       greyMaterial.setSpecularColor(Color.GREY);
+ 
+       // Molecule Hierarchy
+       // [*] moleculeXform
+       //     [*] oxygenXform
+       //         [*] oxygenSphere
+       //     [*] hydrogen1SideXform
+       //         [*] hydrogen1Xform
+       //             [*] hydrogen1Sphere
+       //         [*] bond1Cylinder
+       //     [*] hydrogen2SideXform
+       //         [*] hydrogen2Xform
+       //             [*] hydrogen2Sphere
+       //         [*] bond2Cylinder
+ 
+       Xform moleculeXform = new Xform();
+       Xform oxygenXform = new Xform();
+       Xform hydrogen1SideXform = new Xform();
+       Xform hydrogen1Xform = new Xform();
+       Xform hydrogen2SideXform = new Xform();
+       Xform hydrogen2Xform = new Xform();
+
+      Sphere oxygenSphere = new Sphere(40.0);
+      oxygenSphere.setMaterial(redMaterial);
+
+      Sphere hydrogen1Sphere = new Sphere(30.0);
+      hydrogen1Sphere.setMaterial(whiteMaterial);
+      hydrogen1Sphere.setTranslateX(0.0);
+
+      Sphere hydrogen2Sphere = new Sphere(30.0);
+      hydrogen2Sphere.setMaterial(whiteMaterial);
+      hydrogen2Sphere.setTranslateZ(0.0);
+
+      Cylinder bond1Cylinder = new Cylinder(5, 100);
+      bond1Cylinder.setMaterial(greyMaterial);
+      bond1Cylinder.setTranslateX(50.0);
+      bond1Cylinder.setRotationAxis(Rotate.Z_AXIS);
+      bond1Cylinder.setRotate(90.0);
+
+      Cylinder bond2Cylinder = new Cylinder(5, 100);
+      bond2Cylinder.setMaterial(greyMaterial);
+      bond2Cylinder.setTranslateX(50.0);
+      bond2Cylinder.setRotationAxis(Rotate.Z_AXIS);
+      bond2Cylinder.setRotate(90.0);
+
+      moleculeXform.getChildren().add(oxygenXform);
+      moleculeXform.getChildren().add(hydrogen1SideXform);
+      moleculeXform.getChildren().add(hydrogen2SideXform);
+      oxygenXform.getChildren().add(oxygenSphere);
+      hydrogen1SideXform.getChildren().add(hydrogen1Xform);
+      hydrogen2SideXform.getChildren().add(hydrogen2Xform);
+      hydrogen1Xform.getChildren().add(hydrogen1Sphere);
+      hydrogen2Xform.getChildren().add(hydrogen2Sphere);
+      hydrogen1SideXform.getChildren().add(bond1Cylinder);
+      hydrogen2SideXform.getChildren().add(bond2Cylinder);
+ 
+      hydrogen1Xform.setTx(100.0);
+      hydrogen2Xform.setTx(100.0);
+      hydrogen2SideXform.setRotateY(HYDROGEN_ANGLE);
+
+      moleculeGroup.getChildren().add(moleculeXform);
+
+      world.getChildren().addAll(moleculeGroup);
+}
 }
