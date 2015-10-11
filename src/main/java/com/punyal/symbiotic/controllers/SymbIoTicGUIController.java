36be1ef6edaff9e0b5f807183334110cc25cb95c
Punyal <p.punyal@gmail.com>
2015-10-11 10:51:03 +0200

    angle fix

--- src/main/java/com/punyal/symbiotic/controllers/SymbIoTicGUIController.java
+++ src/main/java/com/punyal/symbiotic/controllers/SymbIoTicGUIController.java
@@ -422,6 +422,7 @@ public class SymbIoTicGUIController implements Initializable {
     private static final double CAMERA_INITIAL_DISTANCE = -500;
     private static final double CAMERA_INITIAL_X_ANGLE = 90.0;
     private static final double CAMERA_INITIAL_Y_ANGLE = -90.0;
+    private static final double CAMERA_INITIAL_Z_ANGLE = 0.0;
     private static final double CAMERA_NEAR_CLIP = 0.1;
     private static final double CAMERA_FAR_CLIP = 10000.0;
     private static final double AXIS_LENGTH = 250.0;
@@ -459,20 +460,21 @@ public class SymbIoTicGUIController implements Initializable {
         handleKeyboard(subSceneWheelLoader, root);
         handleMouse(subSceneWheelLoader, root);
     }
-    
-    
+  
     private void buildCamera() {
         root.getChildren().add(cameraXform);
         cameraXform.getChildren().add(cameraXform2);
         cameraXform2.getChildren().add(cameraXform3);
         cameraXform3.getChildren().add(camera);
         cameraXform3.setRotateZ(180.0);
+        cameraXform3.setRotateY(-20.0);
 
         camera.setNearClip(CAMERA_NEAR_CLIP);
         camera.setFarClip(CAMERA_FAR_CLIP);
         camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
-        cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
         cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+        cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+        cameraXform.rz.setAngle(CAMERA_INITIAL_Z_ANGLE);
     }
     
     private void buildAxes() {
@@ -690,14 +692,17 @@ public class SymbIoTicGUIController implements Initializable {
         
         // radius generation
         
-        wheelModel.getChildren().add(newRadius(greyMaterial, 0.0));
-        for (int i=1; i<18; i++)
+        //wheelModel.getChildren().add(newRadius(greyMaterial, 0.0));
+        for (int i=0; i<18; i++)
             wheelModel.getChildren().add(newRadius(greyMaterial, i*20.0));
         
-        wheelModel.getChildren().add(newRim(greyMaterial, 0.0));
-        for (int i=1; i<360; i++)
+        //wheelModel.getChildren().add(newRim(greyMaterial, 0.0));
+        for (int i=0; i<360; i++)
             wheelModel.getChildren().add(newRim(greyMaterial, i*1.0));
         
+        
+        // red mark
+        wheelModel.getChildren().add(newMark(redMaterial));
         
         wheelGroup.getChildren().add(wheelModel);
         world.getChildren().addAll(wheelGroup);
@@ -758,6 +763,15 @@ public class SymbIoTicGUIController implements Initializable {
         return rad;
     }
     
+    private Xform newMark(final PhongMaterial material) {
+        Box mark = new Box(10, 3, 3);
+        mark.setMaterial(material);
+        mark.setTranslateX(100);
+        
+        Xform markForm = new Xform();
+        markForm.getChildren().add(mark);
+        return markForm;
+    }
     
     private void buildBase() {
         // Materials
